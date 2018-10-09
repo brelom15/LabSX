9ee0a43a17fb2cdeaab60fd5c7e0aeb842e9bce0
brelom15 <lorenz@breitenthaler.at>
2018-10-09 20:19:30 +0200

    Update src/main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,40 @@
-public static void main (){}
+public class Calculator {
+    
+    private final double op1; 
+    private final char op;
+    private final double op2;
+    private final double result;
+
+    public Calculator(double op1, char op, double op2) {
+        this.op1 = op1;
+        this.op = op;
+        this.op2 = op2;
+        
+         switch(op){
+             case '+': result = op1 + op2; break;
+             case '-': result = op1 - op2; break;
+             case '*': result = op1 * op2; break;
+             case '/': result = op1 / op2; break;
+             case '^': result = Math.pow(op1, op2); break;
+             default:
+                 throw new IllegalArgumentException("invailed argument " + op);
+         }
+    }
+
+    public double getOp1() {
+        return op1;
+    }
+
+    public char getOp() {
+        return op;
+    }
+
+    public double getOp2() {
+        return op2;
+    }
+
+    public double getResult() {
+        return result;
+    }
+    
+}
